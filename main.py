3899f32bba56deffa2bb986d37ae591c328b403b
SamHarris2020 <Sam.Harris@digital.ai>
2021-07-20 16:17:23 -0700

    Removed Uneccesarry Files

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+import random
 import discord
+from discord.ext import commands
 
 
+TOKEN = open("token.env").read()  # Never Publish token.env or save it in source control
+
+bot = commands.Bot(command_prefix="!")
 
 
-# Init Discord Bot With Token
-client = discord.Client()
+@bot.command(name='99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+
+bot.run(TOKEN)
